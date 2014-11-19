5bc28fdaec4322f2023ece46945104e6ac193006
onedim <o.nedim@gmail.com>
2014-11-19 18:04:11 +0100

    skontaj ovo

---
+++ ForPetlja.java
@@ -0,0 +1,16 @@
+
+public class ForPetlja {
+
+	public static void main(String[] args) {
+		
+		int br1=TextIO.getInt();
+		int cilj=0;
+		int suma;
+		for (br1=0, br1 > cilj, br1--){// nije dobro
+				br1
+			
+			
+		}
+		
+}
+}
