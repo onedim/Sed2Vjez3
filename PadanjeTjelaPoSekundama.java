73ebaba6fac67635d694a628d0e0bbec9cd45db0
onedim <o.nedim@gmail.com>
2014-11-24 16:51:38 +0100

    oo

--- PadanjeTjelaPoSekundama.java
+++ PadanjeTjelaPoSekundama.java
@@ -2,8 +2,17 @@
 public class PadanjeTjelaPoSekundama {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Unesite zeljenu udaljenost");
+		double mHeight=TextIO.getDouble();
+		double gravity=9.8;
+		System.out.println("Unesite zeljeno vrijeme");
+		double mVrijeme=TextIO.getDouble();
+		double  udaljenost= (Math.pow(mVrijeme, 2)*gravity)/2;
+		do (mHeight!=udaljenost){
+			System.out.println("Tacno ste pogodili vrijeme i vrijeme pada");
+			}
+		 
+			System.out.println("Niste pogodili vrijeme i vrijeme pada");
 	}
 
 }
